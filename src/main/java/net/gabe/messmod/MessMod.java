5853a9e453b76594c5be735ee2dbf038755a5c6b
Gabri <gabril.melton9@gamil.com>
2024-11-24 16:52:03 -0700

    Initial commit

---
+++ src/main/java/net/gabe/messmod/MessMod.java
@@ -0,0 +1,16 @@
+package net.gabe.messmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MessMod implements ModInitializer {
+	public static final String MOD_ID = "messmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
