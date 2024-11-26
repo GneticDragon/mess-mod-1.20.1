ee43822f5f64a78056b3de9d39ef1d8a28b3c7e4
Gabri <gabril.melton9@gamil.com>
2024-11-26 10:06:58 -0700

    New item: Zenith

--- src/main/java/net/gabe/messmod/MessMod.java
+++ src/main/java/net/gabe/messmod/MessMod.java
@@ -2,6 +2,7 @@ package net.gabe.messmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.gabe.messmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class MessMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
