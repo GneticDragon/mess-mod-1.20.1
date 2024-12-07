abcc3fd2adce57c9722015506560e63653aac76b
Gabri <gabril.melton9@gamil.com>
2024-12-06 18:25:26 -0700

    New blocks: Unrefined Block of Zenith and Block of Zenith

--- src/main/java/net/gabe/messmod/MessMod.java
+++ src/main/java/net/gabe/messmod/MessMod.java
@@ -2,6 +2,7 @@ package net.gabe.messmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.gabe.messmod.block.ModBlocks;
 import net.gabe.messmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,6 @@ public class MessMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
